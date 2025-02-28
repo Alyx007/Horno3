9fddd35c619a99f82b4a0bafbf6ef610cd6e5285
BrunoMqzp <a00834415@tec.mx>
2025-02-28 10:16:59 -0600

    App start, and splash screen

---
+++ Horno3/Horno3/Horno3App.swift
@@ -0,0 +1,17 @@
+//
+//  Horno3App.swift
+//  Horno3
+//
+//  Created by Alumno on 28/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Horno3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
