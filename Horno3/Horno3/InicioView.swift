5e9884185fe77d69fd5a731562a37c30339b30af
BrunoMqzp <a00834415@tec.mx>
2025-03-04 17:58:36 -0600

    Create New Tab view, error in InicioView

---
+++ Horno3/Horno3/InicioView.swift
@@ -0,0 +1,18 @@
+//
+//  InicioView.swift
+//  Horno3
+//
+//  Created by Alumno on 04/03/25.
+//
+
+import SwiftUI
+
+struct InicioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InicioView()
+}
---
+++ Horno3/Horno3/Tab.swift
@@ -0,0 +1,18 @@
+//
+//  Tab.swift
+//  Horno3
+//
+//  Created by Alumno on 04/03/25.
+//
+
+import SwiftUI
+
+struct Tab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Tab()
+}
