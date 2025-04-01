ce018d352f88dc6e3bcf6a2eca360e936471b9d4
BrunoMqzp <a00834415@tec.mx>
2025-03-31 19:29:23 -0600

    Indoor Map added

---
+++ Horno3/Horno3/IndoorMap/MapIndoorView.swift
@@ -0,0 +1,34 @@
+//
+//  joprueba.swift
+//  Dinoseum
+//
+//  Created by Alumno on 31/03/25.
+//  Copyright © 2025 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct MapaIndoorView: View {
+    @StateObject var viewModel = IndoorMapViewModel()
+    @Binding var isExpanded: Bool
+    
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+            IndoorMapView(viewModel: viewModel)
+                .edgesIgnoringSafeArea(.all)
+            
+            LevelPicker(viewModel: viewModel)
+                .padding(.top, 60)  // Adjust for safe area
+                .padding(.trailing, 15)
+        }
+    }
+}
+
+//#Preview {
+  //  MapaIndoorView()
+//}
+
+#Preview() {
+    @Previewable @State var isExpanded = true
+    return MapaIndoorView(isExpanded: $isExpanded)
+}
